eebac59da3e1c39c10d45891c37ab97234ce461a
AlexandrMolyavin <117174004+AlexandrMolyavin@users.noreply.github.com>
2022-11-01 17:00:21 +0300

    Update convert-binary-number-in-a-linked-list-to-integer.py

--- Leetcode/convert-binary-number-in-a-linked-list-to-integer.py
+++ Leetcode/convert-binary-number-in-a-linked-list-to-integer.py
@@ -1,14 +1,13 @@
 # Definition for singly-linked list.
 # class ListNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
-
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution:
-    def deleteNode(self, node):
-        """
-        :type node: ListNode
-        :rtype: void Do not return anything, modify node in-place instead.
-        """
-        node.val = node.next.val
-        node.next = node.next.next
+    def getDecimalValue(self, head: ListNode) -> int:
+        current = head
+        a = ""
+        while current:
+            a += str(current.val)
+            current = current.next
+        return int(a,2)
