6e34f80dbdb53e607a907d88f1c240e251c25042
AlexandrMolyavin <117174004+AlexandrMolyavin@users.noreply.github.com>
2022-12-05 20:08:54 +0300

    Update 2418. Sort the People.py

--- Leetcode/Lab 2/2418. Sort the People.py
+++ Leetcode/Lab 2/2418. Sort the People.py
@@ -1,3 +1,4 @@
+#1
 class Solution:
     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
         l=[]
@@ -8,3 +9,32 @@ class Solution:
         for i in l:
             k.append(i[1])
         return k
+ 
+#2 Quick sort
+def sorting(heights):
+    if len(heights) <= 1:
+            return heights
+    left = []
+    right = []
+    center = []
+    elem = heights[0][1]
+    for item in heights:
+        if item[1] < elem:
+            left.append(item)
+        elif item[1] > elem:
+            right.append(item)
+        else:
+            center.append(item)
+
+    return sorting(right) + center + sorting(left)
+    
+    
+class Solution:
+    def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
+       
+        new_array =[(n, h) for n, h in zip(names, heights)]
+       
+ 
+        res = sorting(new_array)
+        
+        return [ n for n, h in res]
