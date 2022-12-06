6833cb3405c0b0d22d42e72b9df960399b21a310
AlexandrMolyavin <117174004+AlexandrMolyavin@users.noreply.github.com>
2022-12-06 22:11:48 +0300

    Update 268. Missing Number.py

--- Leetcode/Lab 2/268. Missing Number.py
+++ Leetcode/Lab 2/268. Missing Number.py
@@ -1,7 +1,9 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        i = 0
-        for i in range(0,len(nums)):
-            if i not in nums:
-                return i
-        return i+1
+        indx_sum = 0
+        nums_sum = 0
+        for i in range(len(nums)):
+            indx_sum += i + 1
+            nums_sum += nums[i]
+
+        return indx_sum-nums_sum
