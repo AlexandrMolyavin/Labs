89c461294ed958247ab20cde2bb71299cf02d368
AlexandrMolyavin <117174004+AlexandrMolyavin@users.noreply.github.com>
2022-12-06 22:56:36 +0300

    Update 977. Squares of a Sorted Array.py

--- Leetcode/Lab 2/977. Squares of a Sorted Array.py
+++ Leetcode/Lab 2/977. Squares of a Sorted Array.py
@@ -2,6 +2,29 @@ class Solution:
 
     def sortedSquares(self, nums: List[int]) -> List[int]:
         for i in range(len(nums)):
-            nums[i] *= nums[i]
-        nums.sort()
-        return nums
+            nums[i] **= 2
+        return sort(nums)
+
+    def sort(nums):
+        if len(nums) ==1:
+            return nums
+        mid = len(nums)//2
+        left = sort(nums[:mid])
+        right = sort(nums[mid:])
+        return merge_two_lst(left,right)
+
+    def merge_two_lst(left,right):
+        c = []
+        i = j =0
+        while i < len(left) and j < len(right):
+            if left[i] < right[j]:
+                c.append(left[i])
+                i += 1
+            else:
+                c.append(right[j])
+                j += 1
+        if i < len(left):
+            c += left[i:]
+        if j < len(right):
+            c += right[j:]
+        return c
